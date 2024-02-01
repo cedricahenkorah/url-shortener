d94c8e10d562947d93752e9760f008a9b35e096a
cedricahenkorah <cedricahenkorah@gmail.com>
2024-02-01 18:06:31 +0000

    root index file set up

--- public/index.php
+++ public/index.php
@@ -1,1 +1,28 @@
 <?php
+
+const BASE_PATH = __DIR__ . '/../';
+
+
+require BASE_PATH . 'vendor/autoload.php';
+
+require BASE_PATH . 'Core/functions.php';
+
+require base_path('bootstrap.php');
+
+$router = new Core\Router();
+
+$routes = require base_path('routes.php');
+
+// parse the uri's
+$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
+
+// routing
+$method = $_POST['_method'] ?? $_SERVER['REQUEST_METHOD'];
+
+$router->route($uri, $method);
+
+// try {
+//     $router->route($uri, $method);
+// } catch (\Throwable $th) {
+//     //throw $th;
+// }
