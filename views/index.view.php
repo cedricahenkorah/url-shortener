c86639ae011a0b04882f003b72966786d57721ab
cedricahenkorah <cedricahenkorah@gmail.com>
2024-02-06 09:36:12 +0000

    web page title changed

--- views/index.view.php
+++ views/index.view.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>SHORTADDY - THE URL SHORTENER</title>
     <script src="https://cdn.tailwindcss.com?plugins=forms"></script>
 </head>
 
